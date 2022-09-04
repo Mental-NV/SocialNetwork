142ab914ffe79ec383dc246777224f81b4db146d
Ruslan Galiev <ruslan.galiev@outlook.com>
2022-09-05 01:07:36 +0200

    Added HTTPS

--- src/WebApps/SocialNetwork.Portal/Program.cs
+++ src/WebApps/SocialNetwork.Portal/Program.cs
@@ -11,7 +11,6 @@ namespace SocialNetwork.Portal
             // Add services to the container.
             builder.Services.AddAuthorization();
 
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
