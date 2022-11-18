7325dc03f01bfc1ddda32db7567b7c50962513c3
Ruslan Galiev <ruslan.galiev@outlook.com>
2022-11-18 02:23:44 +0100

    Added UI

--- src/WebApps/SocialNetwork.Portal/Program.cs
+++ src/WebApps/SocialNetwork.Portal/Program.cs
@@ -1,34 +1,43 @@
-using Microsoft.AspNetCore.Http.Features;
+namespace SocialNetwork.Portal;
 
-namespace SocialNetwork.Portal
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            app.UseAuthorization();
-            app.UseHttpsRedirection();
-
-            app.MapGet("/", (IConfiguration configuration) =>
-            {
-                string hostName = configuration["HOSTNAME"];
-                return $"SocialNetwork.Portal. Renewed SSL certificate. Pod hostname: {hostName}";
-            });
-
-            app.MapGet("/.well-known/acme-challenge/6-HJofuQIiI-uPZC8td1XS4-6mCLPLGhg7Oj7ei6UiA", () =>
-            {
-                return "6-HJofuQIiI-uPZC8td1XS4-6mCLPLGhg7Oj7ei6UiA.QW_WgChS-Jy0OazS-HA49AoTpScnZpEFNVj4JBlJCns";
-            });
-
-            app.Run();
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseAuthorization();
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapGet("/.well-known/acme-challenge/6-HJofuQIiI-uPZC8td1XS4-6mCLPLGhg7Oj7ei6UiA", () =>
+        {
+            return "6-HJofuQIiI-uPZC8td1XS4-6mCLPLGhg7Oj7ei6UiA.QW_WgChS-Jy0OazS-HA49AoTpScnZpEFNVj4JBlJCns";
+        });
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
