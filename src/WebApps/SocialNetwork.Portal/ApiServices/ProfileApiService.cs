6bb66965f593d10c715b57b465cdf5cc78c2b8e6
Ruslan Galiev <ruslan.galiev@outlook.com>
2022-11-30 00:19:12 +0100

    Added a named HttpClient

--- src/WebApps/SocialNetwork.Portal/ApiServices/ProfileApiService.cs
+++ src/WebApps/SocialNetwork.Portal/ApiServices/ProfileApiService.cs
@@ -6,35 +6,16 @@ namespace SocialNetwork.Portal.ApiServices
     public class ProfileApiService : IProfileApiService
     {
         private readonly IHttpClientFactory httpClientFactory;
-        private readonly Uri profileApiBaseUrl;
 
-        public ProfileApiService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        public ProfileApiService(IHttpClientFactory httpClientFactory)
         {
-            if (configuration is null)
-            {
-                throw new ArgumentNullException(nameof(configuration));
-            }
-
             this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
-            var profileApiBaseUrlString = configuration["ApiServices:ProfileApiBaseUrl"];
-
-            if (string.IsNullOrEmpty(profileApiBaseUrlString))
-            {
-                throw new ApplicationException($"{nameof(profileApiBaseUrl)} cannot be null");
-            }
-
-            profileApiBaseUrl = new Uri(profileApiBaseUrlString);
         }
 
         public async Task<IEnumerable<ProfileData>> GetProfilesAsync()
         {
-            using (var httpClient = httpClientFactory.CreateClient())
-            {
-                httpClient.BaseAddress = profileApiBaseUrl;
-                HttpResponseMessage response = await httpClient.GetAsync("/profile", HttpCompletionOption.ResponseHeadersRead);
-                response.EnsureSuccessStatusCode();
-                return await JsonSerializer.DeserializeAsync<IEnumerable<ProfileData>>(await response.Content.ReadAsStreamAsync());
-            }
+            var httpClient = httpClientFactory.CreateClient("ProfileApiClient");
+            return await httpClient.GetFromJsonAsync<IEnumerable<ProfileData>>("/profile");
         }
     }
 }
--- src/WebApps/SocialNetwork.Portal/Program.cs
+++ src/WebApps/SocialNetwork.Portal/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Net.Http.Headers;
 using SocialNetwork.Portal.ApiServices;
 using SocialNetwork.Portal.Middlewares;
 
@@ -15,7 +16,18 @@ public class Program
             .AddAuthorization()
             .AddControllersWithViews()
             .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-        services.AddHttpClient();
+        services.AddHttpClient("ProfileApiClient", (services, client) =>
+        {
+            IConfiguration configuration = services.GetRequiredService<IConfiguration>();
+            var profileApiBaseUrlString = configuration["ApiServices:ProfileApiBaseUrl"];
+            if (string.IsNullOrEmpty(profileApiBaseUrlString))
+            {
+                throw new ApplicationException($"ProfileApi base url cannot be null");
+            }
+            client.BaseAddress = new Uri(profileApiBaseUrlString);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+        });
         services.AddScoped<IProfileApiService, ProfileApiService>();
 
         var app = builder.Build();
