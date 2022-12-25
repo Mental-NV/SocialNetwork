affcbdfcac334b92f7aef62289e2b92fd4b1eb80
Ruslan Galiev <ruslan.galiev@outlook.com>
2022-12-25 13:01:28 +0100

    Added SocialNetwork.IndentityServer project

---
+++ src/WebApps/SocialNetwork.IdentityServer/Program.cs
@@ -0,0 +1,36 @@
+using IdentityServer4.Test;
+
+namespace SocialNetwork.IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddIdentityServer()
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddInMemoryIdentityResources(Config.IdentityResources)
+                //.AddTestUsers(TestUsers.Users)
+                .AddDeveloperSigningCredential();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            app.Run();
+        }
+    }
+}
