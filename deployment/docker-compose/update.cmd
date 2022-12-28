f8e1499ecbacc8d454710b4b8b3b0ac824cf25c5
Ruslan Galiev <ruslan.galiev@outlook.com>
2022-12-28 21:33:06 +0100

    Added IdentityServer as local Kubernetes microservice

--- deployment/docker-compose/update.cmd
+++ deployment/docker-compose/update.cmd
@@ -1,1 +1,1 @@
-docker-compose build && cd.. && cd k8s && kubectl delete -f . && kubectl apply -f .
+docker-compose build && cd.. && cd k8s && kubectl delete -f . && kubectl apply -f . && cd.. && cd docker-compose
--- src/WebApps/SocialNetwork.IdentityServer/Program.cs
+++ src/WebApps/SocialNetwork.IdentityServer/Program.cs
@@ -14,13 +14,33 @@ namespace SocialNetwork.IdentityServer
                 .AddInMemoryClients(Config.Clients)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 //.AddInMemoryIdentityResources(Config.IdentityResources)
-                //.AddTestUsers(TestUsers.Users)
+                .AddTestUsers(TestUsers.Users)
                 .AddDeveloperSigningCredential();
+
+            builder.Services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                options.Secure = CookieSecurePolicy.SameAsRequest;
+                options.OnAppendCookie = cookieContext =>
+                    AuthenticationHelpers.CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext =>
+                    AuthenticationHelpers.CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+            });
 
             var app = builder.Build();
 
+            ILogger<Program> logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+            string basePath = builder.Configuration["BasePath"];
+            logger.LogInformation("BasePath: {basePath}", basePath);
+            if (!string.IsNullOrWhiteSpace(basePath))
+            {
+                app.UsePathBase(basePath);
+            }
+
             app.UseStaticFiles();
             app.UseRouting();
+
             app.UseIdentityServer();
 
             app.UseAuthorization();
@@ -30,7 +50,33 @@ namespace SocialNetwork.IdentityServer
                 endpoints.MapDefaultControllerRoute();
             });
 
+            /*app.MapGet("/api1", (HttpContext ctx, LinkGenerator link)
+                    => $"API1: PathBase: {ctx.Request.PathBase} Path: {ctx.Request.Path} Link: {link.GetPathByName(ctx, "api2", values: null)}")
+                .WithName("api1");
+
+            app.MapGet("/api2", (HttpContext ctx, LinkGenerator link)
+                    => $"API2: PathBase: {ctx.Request.PathBase} Path: {ctx.Request.Path} Link: {link.GetPathByName(ctx, "api1", values: null)}")
+                .WithName("api2");
+
+            app.MapFallback((HttpContext ctx, LinkGenerator link)
+                    => $"FALLBACK: PathBase: {ctx.Request.PathBase} Path: {ctx.Request.Path} Link: {link.GetPathByName(ctx, "api1", values: null)}");
+            */
             app.Run();
         }
+    }
+
+    public static class AuthenticationHelpers
+    {
+        public static void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite != SameSiteMode.None)
+                return;
+            string userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+            if (httpContext.Request.IsHttps && !AuthenticationHelpers.DisallowsSameSiteNone(userAgent))
+                return;
+            options.SameSite = SameSiteMode.Unspecified;
+        }
+
+        public static bool DisallowsSameSiteNone(string userAgent) => userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12") || userAgent.Contains("Macintosh; Intel Mac OS X 10_14") && userAgent.Contains("Version/") && userAgent.Contains("Safari") || userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6");
     }
 }
